1530da62c82858bf84e4375c0762e03175e7d378
Bernardo Marcelo Cani Machado <bernardomarcelocanimachado@gmail.com>
2025-08-05 20:27:07 -0300

    Arquivo DAO modificado

--- src/projetoentra21/UsuarioDAO.java
+++ src/projetoentra21/UsuarioDAO.java
@@ -1,5 +1,16 @@
 package projetoentra21;
 
 public class UsuarioDAO {
+	
+	public void inserirUsuario(Usuario usuario) {
+		
+	}
 
+	public void editarUsuario(Usuario usuario) {
+	
+	}
+		public void deletarUsuario(Usuario usuario) {
+			
+		
+	}
 }
