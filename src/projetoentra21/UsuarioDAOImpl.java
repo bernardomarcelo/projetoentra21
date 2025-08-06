ca9101159498cbed0e08180a12cd6cf0bb4f3353
Bernardo Marcelo Cani Machado <bernardomarcelocanimachado@gmail.com>
2025-08-05 21:49:38 -0300

    Implementação dos métodos do UsuarioDAO

--- src/projetoentra21/UsuarioDAOImpl.java
+++ src/projetoentra21/UsuarioDAOImpl.java
@@ -1,5 +1,37 @@
 package projetoentra21;
 
-public class UsuarioDAOImpl {
+import java.util.List;
+
+public class UsuarioDAOImpl implements UsuarioDAO {
+
+	@Override
+	public void inserirUsuario(Usuario usuario) {
+		
+		
+	}
+
+	@Override
+	public void deletarUsuario(Usuario usuario) {
+	
+		
+	}
+
+	@Override
+	public void editarUsuario(Usuario usuario) {
+		
+		
+	}
+
+	@Override
+	public Usuario recuperarUsuario(Usuario usuario) {
+		
+		return null;
+	}
+
+	@Override
+	public List<Usuario> recuperarUsuarios() {
+		
+		return null;
+	}
 
 }
