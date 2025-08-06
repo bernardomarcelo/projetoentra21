a05680f55fbe5c1d961227c701a536c6220fa146
Bernardo Marcelo Cani Machado <bernardomarcelocanimachado@gmail.com>
2025-08-05 21:34:36 -0300

    Implementação de métodos na Interface UsuarioDAO

--- src/projetoentra21/UsuarioDAO.java
+++ src/projetoentra21/UsuarioDAO.java
@@ -1,5 +1,20 @@
 package projetoentra21;
 
+import java.util.List;
+
 public interface UsuarioDAO {
+	
+		void inserirUsuario (Usuario usuario);
+	
+		void deletarUsuario (Usuario usuario);
+	
+		void editarUsuario (Usuario usuario);
+		
+		Usuario recuperarUsuario(Usuario usuario);
+		
+		List<Usuario> recuperarUsuarios();
+	
+	
+
 
 }
