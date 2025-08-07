758fe82847e3d770b3df21430091d1bfd5c437d2
Bernardo Marcelo Cani Machado <bernardomarcelocanimachado@gmail.com>
2025-08-07 19:00:26 -0300

    Alterações

--- src/projetoentra21/TesteConexao.java
+++ src/projetoentra21/TesteConexao.java
@@ -5,9 +5,9 @@ public class TesteConexao {
 	    public static void main(String[] args) {
 	       
 	    	Usuario usuario = new Usuario("joao", "silva", "jojo", "joao@gmail", "ff");
-	    	Usuario usuario2 = new Usuario("joao", "silva", "jojo", "joao@gmail", "ff");
+	    	Usuario usuario3 = new Usuario("joggo", "silggva", "jggojo", "joaogg@gmail", "fggf");
 	    	UsuarioDAOImpl dao = new UsuarioDAOImpl();
-	    	dao.inserirUsuario(usuario2);
+	    	dao.inserirUsuario(usuario3);
 	    	
 	    	
 	    	
