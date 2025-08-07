0e0b112a48ce0fe0d72853da58aa13ee24f055b5
Bernardo Marcelo Cani Machado <bernardomarcelocanimachado@gmail.com>
2025-08-06 21:41:43 -0300

    atributos, getters, setters e construtores

--- src/projetoentra21/Avaliacao.java
+++ src/projetoentra21/Avaliacao.java
@@ -1,5 +1,70 @@
 package projetoentra21;
 
+
+
 public class Avaliacao {
+	
+	 private long id;
+	 private int nota;
+	 private String descricao;
+	 private Usuario usuario;
+	
+	 private Estabelecimento estabelecimento;
+	 
+	 public Avaliacao() {}
+	 
+	 public Avaliacao(long id, int nota, String descricao, Usuario usuario, Estabelecimento estabelecimento) {
+		 setId(id);
+		 setNota(nota);
+		 setDescricao(descricao);
+		 setUsuario(usuario);
+		 setEstabelecimento(estabelecimento);
+		 
+		 
+	 }
+	 
+	 
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public int getNota() {
+		return nota;
+	}
+
+	public void setNota(int nota) {
+		this.nota = nota;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public Estabelecimento getEstabelecimento() {
+		return estabelecimento;
+	}
+
+	public void setEstabelecimento(Estabelecimento estabelecimento) {
+		this.estabelecimento = estabelecimento;
+	}
+	 
+	 
 
 }
