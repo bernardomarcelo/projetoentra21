ba2212697069e1432c36ac486b79f66637fc5ce2
Bernardo Marcelo Cani Machado <bernardomarcelocanimachado@gmail.com>
2025-08-07 11:56:39 -0300

    teste om banco e término da interface avaliacao

--- src/projetoentra21/AvaliacaoDAO.java
+++ src/projetoentra21/AvaliacaoDAO.java
@@ -1,5 +1,12 @@
 package projetoentra21;
 
+import java.util.List;
+
 public interface AvaliacaoDAO {
 
+	void publicarAvaliacao(Avaliacao avaliacao);
+	void editarAvaliacao(Avaliacao avaliacao);
+	void excluirAvaliacao(long id);
+	Avaliacao recuperarAvaliacao(long id);
+	List<Avaliacao> recuperarAvaliacoes();
 }
--- src/projetoentra21/TesteConexao.java
+++ src/projetoentra21/TesteConexao.java
@@ -5,6 +5,9 @@ public class TesteConexao {
 	    public static void main(String[] args) {
 	       
 	    	Usuario usuario = new Usuario("joao", "silva", "jojo", "joao@gmail", "ff");
+	    	UsuarioDAOImpl dao = new UsuarioDAOImpl();
+	    	dao.inserirUsuario(usuario);
+	    	
 	    	
 	    	
 	    	
