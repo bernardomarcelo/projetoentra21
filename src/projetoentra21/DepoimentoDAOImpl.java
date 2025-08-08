0bc3db01ad43de09268e4eef9ebd8a86a20e86cf
Bernardo Marcelo Cani Machado <bernardomarcelocanimachado@gmail.com>
2025-08-07 21:58:01 -0300

    alterações daodepoimento

---
+++ src/projetoentra21/DepoimentoDAOImpl.java
@@ -0,0 +1,38 @@
+package projetoentra21;
+
+import java.util.List;
+
+public class DepoimentoDAOImpl implements DepoimentoDAO{
+
+	@Override
+	public void inserirDepoimento() {
+		
+		
+		
+	}
+
+	@Override
+	public void deletarDepoimento() {
+		
+		
+	}
+
+	@Override
+	public void editarDepoimento() {
+		
+		
+	}
+
+	@Override
+	public Depoimento recuperarDepoimento() {
+		
+		return null;
+	}
+
+	@Override
+	public List<Depoimento> recuperarDepoimentos() {
+		
+		return null;
+	}
+
+}
