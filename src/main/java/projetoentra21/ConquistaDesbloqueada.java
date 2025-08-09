1ab65d56bc9361b31d2bfacbc847bab6493bce8b
Bernardo Marcelo Cani Machado <bernardomarcelocanimachado@gmail.com>
2025-08-08 21:51:51 -0300

    Expansão da classe, não finalizada

--- src/main/java/projetoentra21/ConquistaDesbloqueada.java
+++ src/main/java/projetoentra21/ConquistaDesbloqueada.java
@@ -1,5 +1,12 @@
 package projetoentra21;
 
 public class ConquistaDesbloqueada {
+	
+	private Long id;
+	private String nomeConquista;
+	private String descricaoConquista;
+	//private Foto foto;
+	
+	public 
 
 }
