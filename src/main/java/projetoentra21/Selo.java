32c183d432cff337c53acbeda0e0af0cbbd1aee3
Bernardo Marcelo Cani Machado <bernardomarcelocanimachado@gmail.com>
2025-08-12 21:29:24 -0300

    Criação da classe Selo

---
+++ src/main/java/projetoentra21/Selo.java
@@ -0,0 +1,27 @@
+package projetoentra21;
+
+public class Selo {
+	
+	private Long id;
+	private String nome;
+	private int pontuacao;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getPontuacao() {
+		return pontuacao;
+	}
+	public void setPontuacao(int pontuacao) {
+		this.pontuacao = pontuacao;
+	}
+
+}
