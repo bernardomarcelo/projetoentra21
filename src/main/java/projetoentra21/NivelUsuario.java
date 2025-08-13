1e4f9dae20159427e543419369542da02d24893d
Bernardo Marcelo Cani Machado <bernardomarcelocanimachado@gmail.com>
2025-08-12 21:21:05 -0300

    Criação da classe NivelUsuario

---
+++ src/main/java/projetoentra21/NivelUsuario.java
@@ -0,0 +1,38 @@
+package projetoentra21;
+
+public class NivelUsuario {
+	
+	private Long id;
+	private String nome;
+	private String descricao;
+	private int pontosNecessarios;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public int getPontosNecessarios() {
+		return pontosNecessarios;
+	}
+	public void setPontosNecessarios(int pontosNecessarios) {
+		this.pontosNecessarios = pontosNecessarios;
+	}
+	
+	void CalcularNivelUsuario() {
+			
+		}
+
+}
